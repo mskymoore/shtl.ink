d05a74ac48f5b8142b44dbd5af4104596d507631
Sky Moore <mskymoore@gmail.com>
2022-06-12 12:56:19 -0500

    make redirect functionality work

---
+++ pages/[short_code].js
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router'
+import redirect from 'nextjs-redirect'
+import Layout from '../components/Layout'
+import styles from '../styles/Layout.module.css'
+
+export default function Url({ url_record }){
+    const router = useRouter();
+    const { short_code } = router.query
+    if (url_record.url){
+        const Redirect = redirect(`http://localhost:8000/${url_record.short_code}`)
+        return <Redirect>
+            <Layout>Redirecting to {url_record.url}!</Layout>
+        </Redirect>
+    }
+    else {
+        return <div className={styles.grid}>
+                    <a className={styles.card}>
+                        <p className={styles.grid}>
+                            <code className={styles.code}>{url_record.message}</code>
+                        </p>
+                    </a>
+                </div>
+    }
+}
+
+
+export async function getServerSideProps({ params }){
+
+    const req = await fetch(`http://localhost:8000/short_code`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(
+                {short_code: params.short_code}
+              )
+        });
+
+    const data = await req.json();
+    return {
+        props: { url_record: data }
+    };
+}
