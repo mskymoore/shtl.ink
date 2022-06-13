aedc10a2e32d8634f09e83f71de962c37afb71ea
Sky Moore <mskymoore@gmail.com>
2022-06-13 15:53:02 -0500

    add dropdown for custom code

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,29 @@
-//import styles from '../styles/Layout.module.css'
-//import styles from '../styles/milligram.css'
+import {useRouter}  from 'next/router'
 
 export default function Home() {
+  const router = useRouter();
   async function create_short_code(event){
     event.preventDefault()
     const url = event.target.url.value
+    var endpoint = ''
+    var body = {}
+    if (event.target.short_code.value != ''){
+        endpoint = 'create_custom_short_code'
+        body = {url: url, short_code: event.target.short_code.value}
+    }
+    else {
+        endpoint = 'create_short_code'
+        body = {url: url}
+    }
 
-    const req = await fetch(`http://localhost:8000/create_short_code`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(
-                {url: url}
-              )
-        });
+    const req = await fetch(`http://localhost:8000/${endpoint}`, {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(body)
+          });
 
     const data = await req.json();
-    alert(`${data.short_code} -> ${data.url}`)
+    router.push(`/short_codes/${data.short_code}`)
   }
 
   return (
@@ -26,6 +34,23 @@ export default function Home() {
           <fieldset>
           <label htmlFor="url">url:</label>
           <input type="text" id="url" name="url" defaultValue="http://superlong.example.url/gobldygoop" />
+          <div className='showHide'>
+            <input type="checkbox" id="toggle" />
+    
+            <label htmlFor="toggle">
+                <span className='expand'>
+                    custom short code
+                    <span className="changeArrow arrow-rt">&nbsp;&gt;</span>
+                    <span className="changeArrow arrow-dn">&nbsp;v</span>
+                </span>
+            </label>
+    
+            <div className="fieldsetContainer">
+                <fieldset id="fdst">
+                <input type="text" id="short_code" name="short_code"/>
+                </fieldset>
+            </div>
+          </div>
           <button className="button-black" type="submit">Create</button>
           </fieldset>
         </form> 
