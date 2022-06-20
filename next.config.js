05ad58773ce3246eb5851e34fe5ad80e50a78f0e
Sky Moore <mskymoore@gmail.com>
2022-06-19 20:11:35 -0500

    remove unneccesary backend config

--- next.config.js
+++ next.config.js
@@ -24,9 +24,7 @@ const nextConfig = {
   },
   env: {
     BASE_URL: process.env["BASE_URL"],
-    API_BASE_URL: process.env["API_BASE_URL"],
-    CORE_CONN_URL: process.env["CORE_CONN_URL"],
-    CORE_API_KEY: process.env["CORE_API_KEY"]
+    API_BASE_URL: process.env["API_BASE_URL"]
   }
 }
 
